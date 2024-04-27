7935b61479ad32456587f7ec6bb8f54fc1d77a1c
rickyreyes10 <rickyreyes2002@gmail.com>
2024-04-27 03:42:35 -0400

    delivery in-progress page
    shows receipt, estimated delivery time and delivery driver interface

---
+++ lib/pages/delivery_progress_page.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_receipt.dart';
+import 'package:food_delivery_app/models/restaurant.dart';
+import 'package:food_delivery_app/services/database/firestore.dart';
+import 'package:provider/provider.dart';
+
+class DeliveryProgressPage extends StatefulWidget {
+  const DeliveryProgressPage({super.key});
+
+  @override
+  State<DeliveryProgressPage> createState() => _DeliveryProgressPageState();
+}
+
+class _DeliveryProgressPageState extends State<DeliveryProgressPage> {
+  //get access to db
+  FirestoreService db = FirestoreService();
+
+  @override
+  void initState() {
+    super.initState();
+
+    //if we get to this page, submit order to firestore db
+    String receipt = context.read<Restaurant>().displayCartReceipt();
+    db.saveOrderToDatabase(receipt);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      bottomNavigationBar: _buildBottomNavBar(context),
+      body: const Column(
+        children: [
+          MyReceipt(),
+        ],
+      ),
+    );
+  }
+
+  //custom Bottom Nav Bar- Message / Call delivery driver
+  Widget _buildBottomNavBar(BuildContext context) {
+    return Container(
+      height: 100,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.secondary,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(40),
+          topRight: Radius.circular(40),
+        ),
+      ),
+      padding: const EdgeInsets.all(25),
+      child: Row(
+        children: [
+          //profile pic of the driver
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.background,
+              shape: BoxShape.circle,
+            ),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.person),
+            ),
+          ),
+
+          const SizedBox(width: 10),
+
+          //driver details
+          Column(
+            children: [
+              Text(
+                "John Doe",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+              Text(
+                "Driver",
+                style: TextStyle(color: Theme.of(context).colorScheme.primary),
+              ),
+            ],
+          ),
+
+          const Spacer(),
+
+          Row(
+            children: [
+              //message button
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.background,
+                  shape: BoxShape.circle,
+                ),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.message),
+                ),
+              ),
+
+              const SizedBox(width: 10),
+
+              //call button
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.background,
+                  shape: BoxShape.circle,
+                ),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.call),
+                  color: Colors.green,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
